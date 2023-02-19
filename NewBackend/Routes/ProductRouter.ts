4f476a09151515e24e7e36b0c6f9be581a3b6871
Roahn <72019287+Roahn@users.noreply.github.com>
2023-02-19 16:08:51 +0530

    added backend code for creating new product

--- NewBackend/Routes/ProductRouter.ts
+++ NewBackend/Routes/ProductRouter.ts
@@ -5,14 +5,15 @@ import {
 } from '../Controllers/ProductController';
 const productRouter = express.Router()
 
-
+import { manage, create } from '../Controllers/ManageController';
 productRouter.get('/:category', (req, res) => {
   getProductByCategory(req, res)
 })
 productRouter.get('/:category/:id', (req, res) => {
   getProductByID(req, res);
 });
-
+productRouter.post('/', create);
+productRouter.get('/',manage);
 
 export default productRouter
 
