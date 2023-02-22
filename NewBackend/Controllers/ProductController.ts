ef7ec7782a84e4e7a73df9356bf9cf6ce0e824f7
preethiashok05 <preethiashok05@gmail.com>
2023-02-22 19:46:33 +0530

    last commit restored

--- NewBackend/Controllers/ProductController.ts
+++ NewBackend/Controllers/ProductController.ts
@@ -64,17 +64,13 @@ export const getProductByCategory = (req: Request, res: Response) => {
 export const getProductByID = (req: Request, res: Response) => {
   const { id } = req.params;
 
-  const query = 'select * ,count(*) as count from products where  prdt_id=' + "'" + id + "'" + ';';
-  db.query(query, (err, result2 :Array<any>) => {
+  const query = 'select * from products where  prdt_id=' + "'" + id + "'" + ';';
+  db.query(query, (err, result2) => {
     if (err) {
       console.log(err);
       return res.status(500).send([]);
     }
     // console.log(result2);
-     let count= result2[0].count;
-    res.header("x-total-count", count);
-    res.header("Access-Control-Expose-Headers", "x-total-count");
-    const result = (count == 0 )?  [] : result2; 
-    return res.status(200).json(result);
+    return res.status(200).json(result2);
   });
 };
